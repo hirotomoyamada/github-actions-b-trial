9068b72870ae0d3bf7283624ab428110c5ff4097
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-30 05:19:47 +0900

    ci: add GitHub Actions workflows and scripts

---
+++ scripts/hoge.ts
@@ -0,0 +1,21 @@
+import { mkdir, readdir, writeFile } from 'fs/promises';
+import path from 'path';
+
+const main = async () => {
+  const files = await readdir(path.join(process.cwd(), '.hoge'));
+
+  await mkdir(path.join(process.cwd(), '.fuga'), { recursive: true });
+
+  await Promise.all(
+    files.map(async (name, index) => {
+      const data = {
+        hoe: 'This is fuga.',
+      };
+
+      const json = JSON.stringify(data);
+      await writeFile(path.join(process.cwd(), '.fuga', `${name}-${index}.json`), json);
+    }),
+  );
+};
+
+main();
